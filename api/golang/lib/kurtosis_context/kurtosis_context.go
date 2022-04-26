a80ac3ec6449caa64337b083e795e563286e970b
galenmarchetti <galenmarchetti@gmail.com>
2022-04-26 05:16:42 -0500

    changing version tag to gmarchetti docker pause unpause

--- api/golang/lib/kurtosis_context/kurtosis_context.go
+++ api/golang/lib/kurtosis_context/kurtosis_context.go
@@ -28,7 +28,7 @@ const (
 	DefaultKurtosisEngineServerGrpcProxyPortNum = uint16(9711)
 
 	// Blank tells the engine server to use the default
-	defaultApiContainerVersionTag = ""
+	defaultApiContainerVersionTag = "gmarchetti/docker-pause-unpause"
 )
 
 var apiContainerLogLevel = logrus.DebugLevel
@@ -264,14 +264,14 @@ func validateEngineApiVersion(ctx context.Context, engineServiceClient kurtosis_
 	doApiVersionsMatch := libraryEngineMajorVersion == runningEngineMajorVersion && libraryEngineMinorVersion == runningEngineMinorVersion
 
 	if !doApiVersionsMatch {
-		 return stacktrace.NewError(
-			 "An API version mismatch was detected between the running engine version '%v' and the engine version this Kurtosis SDK library expects, '%v'. You should:\n" +
-				 "  1) upgrade your Kurtosis CLI to latest using the instructions at https://docs.kurtosistech.com/installation.html\n" +
-				 "  2) use the Kurtosis CLI to restart your engine via 'kurtosis engine restart'\n"	+
-				 "  3) upgrade your Kurtosis SDK library using the instructions at https://github.com/kurtosis-tech/kurtosis-engine-api-lib\n",
-			 runningEngineSemver.String(),
-			 libraryEngineSemver.String(),
-		 )
+		return stacktrace.NewError(
+			"An API version mismatch was detected between the running engine version '%v' and the engine version this Kurtosis SDK library expects, '%v'. You should:\n"+
+				"  1) upgrade your Kurtosis CLI to latest using the instructions at https://docs.kurtosistech.com/installation.html\n"+
+				"  2) use the Kurtosis CLI to restart your engine via 'kurtosis engine restart'\n"+
+				"  3) upgrade your Kurtosis SDK library using the instructions at https://github.com/kurtosis-tech/kurtosis-engine-api-lib\n",
+			runningEngineSemver.String(),
+			libraryEngineSemver.String(),
+		)
 	}
 
 	return nil
